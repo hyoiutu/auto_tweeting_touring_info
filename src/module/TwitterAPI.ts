d78aaaf06d5869163ec1af9794f3ece2b9f0e768
hyoiutu <keisuke495500@gmail.com>
2022-06-02 04:58:13 +0900

    TwitterAPIのアクセストークンをリフレッシュするようにした

--- src/module/TwitterAPI.ts
+++ src/module/TwitterAPI.ts
@@ -1,10 +1,6 @@
 import { twitterAxios } from "../axios";
 import fs from "fs";
-import {
-  CLIENT_ID,
-  CLIENT_SECRET,
-  REFRESH_TOKEN,
-} from "../../constants/twitterAPI";
+import { CLIENT_ID, CLIENT_SECRET } from "../../constants/twitterAPI";
 
 export class TwitterAPI {
   private clientId: string;
@@ -14,34 +10,38 @@ export class TwitterAPI {
   constructor() {
     this.clientId = CLIENT_ID;
     this.clientSecret = CLIENT_SECRET;
-    this.refreshToken = REFRESH_TOKEN;
+    this.refreshToken = "";
     this.accessToken = "";
   }
 
   public static async build() {
     const twitterAPI = new TwitterAPI();
-    const accessToken = await twitterAPI.readAccessToken();
-    // TODO: accessTokenが失効してそうであればこの辺の実装を進める
-    /*
-    const isExpired = await twitterAPI.isExpiredAccessToken(accessToken);
+    twitterAPI.accessToken = twitterAPI.readAccessToken();
+    twitterAPI.refreshToken = twitterAPI.readRefreshToken();
+
+    const isExpired = await twitterAPI.isExpiredAccessToken();
     if (isExpired) {
       await twitterAPI.refreshAccessToken();
-      await twitterAPI.writeAccessToken(twitterAPI.accessToken);
-    } else {
-      twitterAPI.accessToken = accessToken;
+
+      twitterAPI.writeAccessToken(twitterAPI.accessToken);
+      twitterAPI.writeRefreshToken(twitterAPI.refreshToken);
     }
-    */
-    twitterAPI.accessToken = accessToken;
 
     return twitterAPI;
   }
 
   public async getMyInfo() {
-    const res = await twitterAxios.get("/users/me", {
-      headers: {
-        Authorization: `Bearer ${this.accessToken}`,
-      },
-    });
+    const res = await twitterAxios
+      .get("/users/me", {
+        headers: {
+          Authorization: `Bearer ${this.accessToken}`,
+        },
+      })
+      .then((res) => res)
+      .catch((err) => {
+        throw err;
+      });
+
     return res.data.data;
   }
 
@@ -58,50 +58,66 @@ export class TwitterAPI {
     console.log("tweeted");
   }
 
-  /*
-  private async isExpiredAccessToken(accessToken: string) {
-    return await instance
-      .get("/athlete", {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      })
-      .then(() => {
-        return false;
-      })
+  private async isExpiredAccessToken() {
+    return await this.getMyInfo()
+      .then(() => false)
       .catch((err) => {
-        if (err.response.status === 401) {
-          if (err.response.data.errors.length > 0) {
-            const reason = err.response.data.errors[0];
-            if (reason.field === "access_token" && reason.code === "invalid") {
-              return true;
-            }
-          }
+        if (
+          err.response.data &&
+          err.response.data.status === 401 &&
+          err.response.data.title === "Unauthorized"
+        ) {
+          return true;
         }
+
         throw err;
       });
   }
 
   private async refreshAccessToken() {
-    const res = await instance.post(`/oauth/token?client_id=${this.clientId}`, {
-      client_secret: this.clientSecret,
-      grant_type: "refresh_token",
-      refresh_token: this.refreshToken,
-    });
+    const res = await twitterAxios
+      .post(
+        "/oauth2/token",
+        {
+          client_id: this.clientId,
+          grant_type: "refresh_token",
+          refresh_token: this.refreshToken,
+        },
+        {
+          auth: {
+            username: this.clientId,
+            password: this.clientSecret,
+          },
+        }
+      )
+      .then((res) => res)
+      .catch((err) => {
+        throw err;
+      });
 
     this.accessToken = res.data.access_token as string;
-  }
-  */
+    this.refreshToken = res.data.refresh_token as string;
 
-  private async readAccessToken() {
+    console.log("Twitter API access token was refreshed");
+  }
+
+  private readAccessToken() {
     const accessToken = fs.readFileSync(
       "secrets/twitterAPIAccessToken",
       "utf-8"
     );
     return accessToken;
   }
-  /*
-  private async writeAccessToken(accessToken: string) {
+
+  private readRefreshToken() {
+    const refreshToken = fs.readFileSync(
+      "secrets/twitterAPIRefreshToken",
+      "utf-8"
+    );
+    return refreshToken;
+  }
+
+  private writeAccessToken(accessToken: string) {
     try {
       fs.writeFileSync("secrets/twitterAPIAccessToken", accessToken);
       console.log("write end");
@@ -109,5 +125,13 @@ export class TwitterAPI {
       console.error(err);
     }
   }
-  */
+
+  private writeRefreshToken(refreshToken: string) {
+    try {
+      fs.writeFileSync("secrets/twitterAPIRefreshToken", refreshToken);
+      console.log("write end");
+    } catch (err) {
+      console.error(err);
+    }
+  }
 }
