fd72d49d2b512a37699803b88d9f7fb7d3182c64
hyoiutu <keisuke495500@gmail.com>
2022-06-18 22:26:24 +0900

    テスト実行前にテスト用のファイルを入れるディレクトリを作るようにした

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,4 +1,7 @@
 import dotenv from "dotenv";
+import { existsSync } from "fs";
+import { execSync } from "child_process";
+import { getEnv } from "./src/util/env";
 
 export default () => {
   dotenv.config();
@@ -8,5 +11,14 @@ export default () => {
     STRAVA_API_ACCESS_TOKEN: "example-access-token",
     TWITTER_API_ACCESS_TOKEN: "example-access-token",
     TWITTER_API_REFRESH_TOKEN: "example-refresh-token",
+    TEST_FILES_DIR: "./testFiles",
   };
+
+  const testDir = getEnv("TEST_FILES_DIR");
+
+  if (!existsSync(testDir)) {
+    execSync(`mkdir ${testDir}`);
+  } else {
+    execSync(`rm ${testDir}/*`);
+  }
 };
