2b5455f7f89149cbf2e502008adbb14cc1d1a008
hyoiutu <keisuke495500@gmail.com>
2022-06-22 19:21:01 +0900

    tweetを生成する部分は別ファイルに移した

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,19 @@
 import { GoogleMapStaticAPI } from "./module/GoogleMapStaticAPI";
 import { StravaAPI } from "./module/StravaAPI";
 import { TwitterAPI } from "./module/TwitterAPI";
-import { writeAPIResToJSON, readJSONFromFile, svgToPng } from "./util/file";
+import { writeAPIResToJSON, readJSONFromFile } from "./util/file";
 import { generateTweetByActivityId } from "./util/tweet";
-import * as d3 from "d3";
-import * as topojson from "topojson";
-import fs from "fs";
-import { JSDOM } from "jsdom";
-import {
-  Feature,
-  Point,
-  FeatureCollection,
-  GeoJsonProperties,
-  MultiPolygon,
-  Geometry,
-  GeometryObject,
-  MultiPoint,
-  LineString,
-  MultiLineString,
-  Polygon,
-  GeometryCollection,
-  Position,
-  BBox,
-} from "geojson";
-import { generateSVGByRegions, readTopoJSON } from "./svg/svg";
-import { getCityByLatLng } from "./svg/latlng";
+
 import dotenv from "dotenv";
 import { overWrittenSecretsEnvs, setSecretsEnvs } from "./util/env";
-import { arrayBuffer } from "stream/consumers";
 async function main() {
   dotenv.config();
   setSecretsEnvs("./secrets");
 
-  // const stravaAPI = await StravaAPI.build();
+  const stravaAPI = await StravaAPI.build();
   const twitterAPI = await TwitterAPI.build();
-  // const googleMapStaticAPI = GoogleMapStaticAPI.build();
-  /*
+  const googleMapStaticAPI = GoogleMapStaticAPI.build();
+
   let activities;
 
   if (process.argv.length < 3 || process.argv[2] === "api") {
@@ -48,62 +26,19 @@ async function main() {
     activities = JSON.parse(readJSONFromFile("./json/latest_activities.json"));
   }
 
-  const cities: string[] = [];
+  const latestActivity = activities[0];
 
-  for (const activity of activities) {
-    const activityDetail = await stravaAPI.getActivityDetailById(activity.id);
+  const tweetStatus = await generateTweetByActivityId(
+    stravaAPI,
+    googleMapStaticAPI,
+    latestActivity.id,
+    0
+  );
 
-    await googleMapStaticAPI.getRouteMap(
-      activityDetail.map.summary_polyline,
-      `./routeImg/${activityDetail.start_date_local}_${activityDetail.name}.jpg`
-    );
-
-    cities.push(
-      await getCityByLatLng({
-        lat: activity.start_latlng[1],
-        lng: activity.start_latlng[0],
-      })
-    );
-    cities.push(
-      await getCityByLatLng({
-        lat: activity.end_latlng[1],
-        lng: activity.end_latlng[0],
-      })
-    );
-  }
-
-  console.log({ cities });
-
-  await generateSVGByRegions(cities, "./svg/hoge.svg", {
-    plotArea: "regions",
-    margin: 10,
-  });
-  await svgToPng("./svg/hoge.svg", "./png/margin.png");
-
-  /*
-  let sumDistance = 0;
-  for (const activity of activities) {
-    const tweet = await generateTweetByActivityId(
-      stravaAPI,
-      activity.id,
-      sumDistance
-    );
-    sumDistance += activity.distance / 1000;
-    console.log("~~~~~~~~~~~~~~~~~");
-    console.log(tweet);
-    console.log("~~~~~~~~~~~~~~~~~");
-  }
-  */
-  // MEMO: 本当にTweetを試したいときだけ使う
-  const randomString = [...Array(10)]
-    .map(() => {
-      return String.fromCharCode(Math.floor(Math.random() * (126 - 35)) + 35);
-    })
-    .join("");
-  await twitterAPI.oldClientTweet(`ひょいうつ！${randomString}`, [
-    "./png/margin.png",
-    "routeImg/2021-12-31T08:59:32Z_Morning サイクリング.jpg",
-  ]);
+  await twitterAPI.oldClientTweet(
+    tweetStatus.tweet,
+    tweetStatus.mediasFilePath
+  );
 }
 
 main()
--- src/util/tweet.ts
+++ src/util/tweet.ts
@@ -1,26 +1,80 @@
+import { GoogleMapStaticAPI } from "../module/GoogleMapStaticAPI";
 import { StravaAPI } from "../module/StravaAPI";
-import { getCityByLatLng } from "./staravaData";
+import { getCityByLatLng } from "../svg/latlng";
+import polyline from "@mapbox/polyline";
+import { generateSVGByRegions } from "../svg/svg";
+import { svgToPng } from "./file";
 
 export async function generateTweetByActivityId(
   stravaAPI: StravaAPI,
+  googleMapStaticAPI: GoogleMapStaticAPI,
   activityId: string,
   sumDistance: number
 ) {
   const activity = await stravaAPI.getActivityDetailById(activityId);
-  const distance_km = activity.distance / 1000;
+
+  const fileName = `${activity.start_date_local}_${activity.name}`;
+
+  const routeImg = `./routeImg/${fileName}.jpg`;
+  await googleMapStaticAPI.getRouteMap(activity.map.summary_polyline, routeImg);
+
+  const routeLatLngs = thinOut(
+    polyline.decode(activity.map.summary_polyline),
+    10
+  );
+
+  const cities = uniq(
+    await Promise.all(
+      routeLatLngs.map(async (latlng) => {
+        return await getCityByLatLng({
+          lat: latlng[0],
+          lng: latlng[1],
+        });
+      })
+    )
+  );
+
+  const distanceKm = Math.round(activity.distance) / 1000;
+
   const startCity = await getCityByLatLng({
-    lat: activity.start_latlng[1],
-    lng: activity.start_latlng[0],
+    lat: activity.start_latlng[0],
+    lng: activity.start_latlng[1],
   });
   const endCity = await getCityByLatLng({
-    lat: activity.end_latlng[1],
-    lng: activity.end_latlng[0],
+    lat: activity.end_latlng[0],
+    lng: activity.end_latlng[1],
   });
-  const tweet = `
-    ${startCity}~${endCity}
-    走行距離: ${distance_km}km
-    総距離: ${distance_km + sumDistance}km
-  `;
 
-  return tweet;
+  const startEndCitiesText = `${startCity}~${endCity}`;
+  const distanceKmText = `走行距離: ${distanceKm}km`;
+  const sumDistanceText = `総距離: ${distanceKm + sumDistance}km`;
+  const citiesText = `通過した市町村:\n${cities.join("\n")}`;
+
+  const tweet = [
+    startEndCitiesText,
+    distanceKmText,
+    sumDistanceText,
+    citiesText,
+  ].join("\n");
+
+  const svgPath = `./svg/${fileName}.svg`;
+  const pngPath = `./png/${fileName}.png`;
+
+  await generateSVGByRegions(cities, svgPath, {
+    plotArea: "regions",
+    margin: 10,
+    width: 1200,
+    height: 1200,
+  });
+  await svgToPng(svgPath, pngPath);
+
+  return { tweet, mediasFilePath: [routeImg, pngPath] };
+}
+
+function thinOut<T>(arr: Array<T>, thinOutRatio: number) {
+  return arr.filter((_, i) => i % thinOutRatio === 0);
+}
+
+function uniq<T>(arr: Array<T>) {
+  return [...new Set(arr)];
 }
